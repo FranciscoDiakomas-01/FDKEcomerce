a13ff065dad88c4d8601bd36b6c9dd3da0708790
Francisco <franciscodiakoma@gmail.com>
2024-07-22 03:37:08 +0100

    quase terminando

---
+++ scrollRevel.js
@@ -0,0 +1,58 @@
+ScrollReveal({
+    delay : 1000,
+    origin : "rigth",
+    distance : "50px",
+    duration: 500,
+    reset : true
+}).reveal('.home');
+
+ScrollReveal({
+    delay : 500,
+    origin : "bottom",
+    distance : "50px",
+    duration: 500,
+    reset : true
+}).reveal('.textoMain');
+
+ScrollReveal({
+    delay : 900,
+    origin : "rigth",
+    distance : "10px",
+    duration: 500,
+    reset : true
+}).reveal(".cardGlide");
+
+ScrollReveal({
+    delay : 500,
+    origin : "left",
+    distance : "100px",
+    duration: 500,
+    reset : true
+}).reveal(".marcas");
+
+ScrollReveal({
+    delay : 500,
+    origin : "left",
+    distance : "150px",
+    duration: 700,
+    reset : true
+}).reveal(".pagamentos");
+
+
+ScrollReveal({
+    delay : 1000,
+    origin : "rigth",
+    distance : "150px",
+    duration: 700,
+    reset : true
+}).reveal(".subscreve");
+
+ScrollReveal({
+    delay : 500,
+    origin : "left",
+    distance : "150px",
+    duration: 700,
+    reset : true
+}).reveal(".rodape");
+
+
