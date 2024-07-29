dbaa33f352a326a2c4383bed17ecfc1b827e2f3f
Francisco <franciscodiakoma@gmail.com>
2024-07-29 07:59:04 +0100

    actualizando

---
+++ search.js
@@ -0,0 +1,29 @@
+
+
+let categories = document.querySelectorAll("ul a")
+
+let searchTitle = document.getElementById("searchTitle")
+
+
+categories.forEach(category =>{
+    category.addEventListener("click",Active)
+})
+
+
+function Active(e){
+    categories.forEach(category =>{
+
+        category.classList.remove("active")
+    })
+    this.classList.add("active")
+    searchTitle.textContent = e.target.textContent
+    let title= e.target.textContent
+    if(title == "Todos"){
+        return
+    }else{
+        
+        //API
+        alert(title)
+    }
+
+}
